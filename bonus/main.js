7cf8e9e9aa029efb7d92302593bf88975d918f70
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-21 18:02:44 +0100

    Corezzione accesso alle proprietá

--- bonus/main.js
+++ bonus/main.js
@@ -58,7 +58,7 @@ function divUser(memberObj) {
 
     // crea img
     createImage = document.createElement('img');
-    createImage.src = 'img/' + memberObj['image'];
+    createImage.src = 'img/' + memberObj.image;
 
     // crea un nuovo div con i test
     divText = document.createElement('div');
@@ -69,8 +69,8 @@ function divUser(memberObj) {
     roleH3 = document.createElement('h3');
 
     // inserisce i testi nel div inetrno
-    nameH2.append(`${memberObj['name']}`);
-    roleH3.append(`${memberObj['role']}`);
+    nameH2.append(`${memberObj.name}`);
+    roleH3.append(`${memberObj.role}`);
     divText.append(nameH2, roleH3);
 
     // inserisce gli elementi nel DOM
